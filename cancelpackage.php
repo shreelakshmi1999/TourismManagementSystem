0950ba992b2cb56c035173013c9fce4c279f1b6b
shreelakshmi1999 <69152006+shreelakshmi1999@users.noreply.github.com>
2020-10-01 17:56:53 +0530

    Update cancelpackage.php

--- cancelpackage.php
+++ cancelpackage.php
@@ -1,5 +1,6 @@
 <?php
 include("connection.php");
+//This allows the storeg info of the packages with refference to their id being deleted
 $new1=$_GET['packageid'];
 $sql="delete from packages where packageid=".$_GET['packageid'];
 $result=mysqli_query($a,$sql);
